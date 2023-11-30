f24c87b12948eafeafc1465b79d9ac26c9a21e57
onexsan <onexsan@gmail.com>
2023-11-30 18:49:12 +0400

    DEVS-6264: added ts support

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
 const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: {
     'buy-crypto': './src/buy-crypto.js',
     'buy-smart-contract': './src/buy-smart-contract.js',
     'buy-nft': './src/buy-nft.js',
+    'ts/buy-crypto': './src/ts/buy-crypto.ts',
+    'ts/buy-smart-contract': './src/ts/buy-smart-contract.ts',
+    'ts/buy-nft': './src/ts/buy-nft.ts',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
+    new CleanWebpackPlugin(), 
     new CopyPlugin({
       patterns: [
         { from: 'static' },
