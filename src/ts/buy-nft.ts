efc0d795c776688fc494a796a1edeab30d0a03f8
onexsan <onexsan@gmail.com>
2023-11-30 18:49:12 +0400

    DEVS-6264: added ts support

---
+++ src/ts/buy-nft.ts
@@ -0,0 +1,83 @@
+/* eslint-disable no-undef */
+import WertWidget from '@wert-io/widget-initializer';
+import { signSmartContractData } from '@wert-io/widget-sc-signer';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Buffer } from 'buffer/';
+
+type WindowType = Window & typeof globalThis & {
+  Buffer: any;
+  ethereum: any;
+}
+
+(window as WindowType).Buffer = Buffer; // needed to use `signSmartContractData` in browser
+
+/* We advise you not to use the private key on the frontend
+    It is used only as an example
+*/
+
+if ((window as any).ethereum) {
+  (async () => {
+    // Get user address
+
+    const userAccounts = await (window as WindowType).ethereum.request({method: 'eth_requestAccounts'});
+    
+    //@ts-ignore
+    const web3: any = new Web3((window as WindowType).ethereum)
+    const userAddress = userAccounts[0];
+    // Encode the call to mintNFT(address = userAddress, numberOfTokens = 1)
+    const sc_input_data = web3.eth.abi.encodeFunctionCall({
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "numberOfTokens",
+          "type": "uint256"
+        }
+      ],
+      "name": "mintNFT",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    }, [userAddress, 1]);
+    const privateKey = '0x57466afb5491ee372b3b30d82ef7e7a0583c9e36aef0f02435bd164fe172b1d3';
+    // Create signed SC data for wert-widget
+    // Please do this on backend
+    const signedData = signSmartContractData({
+      address: userAddress, // user's address
+      commodity: 'MATIC',
+      commodity_amount: 3, // the crypto amount that should be sent to the contract method
+      network: 'mumbai',
+      sc_address: '0x6af35a72b2490a44c0e88ae635b9b38516544db1', // your SC address
+      sc_input_data,
+    }, privateKey);
+    const otherWidgetOptions = {
+      partner_id: '01GCRJZ1P7GP32304PZCS6RSPD', // your partner id
+      click_id: uuidv4(), // unique id of purhase in your system
+      origin: 'https://sandbox.wert.io', // this option needed only in sandbox
+    };
+    const nftOptions = {
+      extra: {
+        item_info: {
+          author: "Wert",
+          image_url:
+            "http://localhost:8765/sample_nft.png",
+          name: "Wert Sample NFT",
+          seller: "Wert",
+        }
+      },
+    };
+
+    const wertWidget = new WertWidget({
+      ...signedData,
+      ...otherWidgetOptions,
+      ...nftOptions,
+    });
+
+    wertWidget.mount();
+  })()
+}
---
+++ src/ts/buy-smart-contract.ts
@@ -0,0 +1,40 @@
+import WertWidget from '@wert-io/widget-initializer';
+import { signSmartContractData } from '@wert-io/widget-sc-signer';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Buffer } from 'buffer/';
+
+type WindowType = Window & typeof globalThis & {
+  Buffer: any;
+  ethereum: any;
+}
+
+(window as WindowType).Buffer = Buffer; // needed to use `signSmartContractData` in browser
+
+/* We advise you not to use the private key on the frontend
+    It is used here for example only
+*/
+const privateKey = '0x57466afb5491ee372b3b30d82ef7e7a0583c9e36aef0f02435bd164fe172b1d3';
+const signedData = signSmartContractData({
+  address: '0x96D5990185022212d367A0e09263B12Dbb4EE06A',
+  commodity: 'ETH',
+  network: 'goerli',
+  commodity_amount: 0.3,
+  sc_address: '0x3b2305502bd6f8b1eb2ed474ac15c61c6702b18b',
+  sc_input_data: '0x9dae76ea000000000000000000000000000000000000000000000000000000000000003700000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001',
+}, privateKey);
+const otherWidgetOptions = {
+  partner_id: '01GCRJZ1P7GP32304PZCS6RSPD',
+  click_id: uuidv4(), // unique id of purhase in your system
+  origin: 'https://sandbox.wert.io', // this option needed only for this example to work
+  listeners: {
+    loaded: () => console.log('loaded'),
+  },
+};
+
+const wertWidget = new WertWidget({
+  ...signedData,
+  ...otherWidgetOptions,
+});
+
+wertWidget.mount();
