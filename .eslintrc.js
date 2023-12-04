0fdc8e8cc7d23c26f2af54946df9c4b6e959718c
onexsan <onexsan@gmail.com>
2023-12-04 11:36:17 +0400

    DEVS-6264: added typescript-eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,22 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  // parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2018,
   },
-  extends: 'eslint:recommended',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'semi': [2, 'always'],
+    'quotes': [2, 'single', { 'avoidEscape': true }]
+  }
 };
