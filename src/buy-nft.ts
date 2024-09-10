b3b8bf871e57d37dc0c2252ef61aaa8c07b37a49
maksimdashko <kense1maks@gmail.com>
2024-09-10 18:30:27 +0300

    DEVS-7522: after rebase update;

--- src/buy-nft.ts
+++ src/buy-nft.ts
@@ -50,9 +50,9 @@ if (window.ethereum) {
     const signedData = signSmartContractData(
       {
         address: userAddress, // user's address
-        commodity: 'POL',
+        commodity: 'MATIC',
         commodity_amount: 3, // the crypto amount that should be sent to the contract method
-        network: 'amoy',
+        network: 'amoi',
         sc_address: '0xAAC496808A678B834073FB3435857FdcF0dc186F', // your SC address
         sc_input_data,
       },
