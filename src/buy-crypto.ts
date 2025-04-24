6bde9e719688202b1525fbbd7a214c06ba9d8573
duzza89 <109074484+duzza89@users.noreply.github.com>
2025-04-24 11:34:34 +0300

    Update buy-crypto.ts
    partner id changed from default

--- src/buy-crypto.ts
+++ src/buy-crypto.ts
@@ -3,7 +3,7 @@ import type { Options } from '@wert-io/widget-initializer/types';
 import { v4 as uuidv4 } from 'uuid';
 
 const options: Options = {
-  partner_id: 'default',
+  partner_id: '01GCRJZ1P7GP32304PZCS6RSPD',
   click_id: uuidv4(), // unique id of purсhase in your system
   origin: 'https://sandbox.wert.io', // this option needed only in sandbox
   commodity: 'ETH',
